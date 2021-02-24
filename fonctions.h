a425afa03a1d45d4f8e103ef6fe0a71887709892
Sonnard Joel <joel.sonnard@heig-vd.ch>
2021-02-24 15:31:22 +0000

    Fin du 24.02

---
+++ fonctions.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+
+
+//proto fonctions
+void ex1(void);
+void ex2(void);
--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
+//programme de base pour tests
 
+
+
+#include <iostream>
+#include "fonctions.h"
 using namespace std;
 
 int main()
 {
     int a = 0;
     cout << "Bonjour, a = " << a << endl;
+    ex1();
+    ex2();
 }
